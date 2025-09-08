f5c266ea25c2e7352ad596647278c949764b3607
Devdanny v2 <solvvitalik@gmail.com>
2025-09-08 18:03:03 +0100

    Movie file

--- frontend/src/data/movies.js
+++ frontend/src/data/movies.js
@@ -1,14 +1,15 @@
 export const movies = [
   {
     id: "1",
-    title: "Test Movie",
-    description: "A short demo movie",
-    thumbnailUrl: "/videos/test.jpg", // optional thumbnail you create
+    title: "Big Buck Bunny",
+    description: "A short open movie by the Blender Institute, licensed under CC-BY.",
+    thumbnailUrl: "https://peach.blender.org/wp-content/uploads/poster_bunny_small.jpg",
     videoUrls: [
-      { quality: "720p", url: "/videos/output.mp4" }
+      { quality: "720p", url: "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_640x360.m4v" },
+      { quality: "1080p", url: "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_1280x720.mp4" }
     ],
     subtitles: [
-      { lang: "en", url: "/videos/test.vtt" } // optional subtitle file
+      { lang: "en", url: "https://raw.githubusercontent.com/PolyAI-LDN/video-subtitles/master/bbb_subtitles_en.vtt" }
     ],
   },
 ];
